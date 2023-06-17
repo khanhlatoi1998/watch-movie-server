1288c4d0c95cd7cdf74309d2fda76661ee3f9ca0
khanhngo.pix@gmail.com <khanhngo.pix@gmail.com>
2023-06-17 18:51:17 +0700

    u

--- config/firebaseStorage.js
+++ config/firebaseStorage.js
@@ -2,6 +2,8 @@ import admin from 'firebase-admin';
 import dotenv from "dotenv";
 import serviceAccount from '../creditial.json' assert  {type: 'json'};
 
+dotenv.config();
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
