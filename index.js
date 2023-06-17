d6949c4077af1907b92b13d2dfecdf13828b0676
khanhngo.pix@gmail.com <khanhngo.pix@gmail.com>
2023-06-17 18:34:51 +0700

    u

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ import dotenv from 'dotenv';
 import { errorHandler } from './middlewares/Error.middlewares.js';
 import express from 'express';
 import userRouter from './routes/Users.router.js';
-// import movieRouter from './routes/Movies.router.js';
-// import categoryRouter from './routes/Categories.router.js';
-// import UploadRouter from './controllers/UploadFire.js';
+import movieRouter from './routes/Movies.router.js';
+import categoryRouter from './routes/Categories.router.js';
+import UploadRouter from './controllers/UploadFire.js';
 
 dotenv.config();
 const app = express();
