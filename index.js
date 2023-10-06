6043300796b03e8fddbd35017cb3c6c5fe73db5e
khanhngo.pix@gmail.com <khanhngo.pix@gmail.com>
2023-10-06 23:46:19 +0700

    u

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import bodyParser from 'body-parser';
-import { connectDB } from './config/db.js';
+// import { connectDB } from './config/db.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { errorHandler } from './middlewares/Error.middlewares.js';
+// import { errorHandler } from './middlewares/Error.middlewares.js';
 import express from 'express';
 import userRouter from './routes/Users.router.js';
 import movieRouter from './routes/Movies.router.js';
 import categoryRouter from './routes/Categories.router.js';
 import UploadRouter from './controllers/UploadFile.js';
 
-dotenv.config();
+// dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -19,13 +19,13 @@ app.use(bodyParser.urlencoded({ extended: false, limit: '3000mb' }));
 app.use(cors());
 
 // connect DB
-connectDB();
+// connectDB();
 
-app.use(errorHandler);
-app.use('/api/users', userRouter);
-app.use('/api/movies', movieRouter);
-app.use('/api/categories', categoryRouter);
-app.use('/api/upload', UploadRouter);
+// app.use(errorHandler);
+// app.use('/api/users', userRouter);
+// app.use('/api/movies', movieRouter);
+// app.use('/api/categories', categoryRouter);
+// app.use('/api/upload', UploadRouter);
 
 app.get('/', (req, res) => {
     res.send('start server')
